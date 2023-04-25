50d5591fb517272b11f9fe30faed804889892f96
Angel Casapía <angel.casapia@gmail.com>
2023-04-25 17:52:40 -0500

    network feature

--- hooks/android/build_gradle_add_dependency.js
+++ hooks/android/build_gradle_add_dependency.js
@@ -1,50 +1,49 @@
 #!/usr/bin/env node
+module.exports = function () {
+    const fs = require('fs'),
+        os = require("os"),
+        readline = require("readline"),
+        deferral = require('q').defer();
 
-module.exports = function(ctx) {
-    var fs = require('fs'),
-    os = require("os"),
-    readline = require("readline"),
-    deferral = require('q').defer();
+    const perfPluginStr = "apply plugin: 'com.google.firebase.firebase-perf'"
+    let perfPluginStrExists = false
+    const classpathsStrToVerify = "com.google.firebase:perf-plugin:1.4.2"
+    const classpathsStr = `\t\tclasspath "${classpathsStrToVerify}"`
+    const rootBuildGradlePath = "platforms/android/build.gradle"
+    const appBuildGradlePath = "platforms/android/app/build.gradle"
 
-var googleServicesStr = "if (project.extensions.findByName('googleServices') == null) { apply plugin: 'com.google.gms.google-services' }"
-var googleServicesStrExists = false
-var classpathsStrToVerify = "com.google.gms:google-services:4.3.10"
-var classpathsStr = '\t\tclasspath "com.google.gms:google-services:4.3.10"'
-var rootBuildGradlePath = "platforms/android/build.gradle"
-var appBuildGradlePath = "platforms/android/app/build.gradle"
-
-var lineReader = readline.createInterface({
-    terminal: false,
-    input : fs.createReadStream(rootBuildGradlePath)
-});
-lineReader.on("line", function(line) {
-    if (!line.includes(classpathsStrToVerify)) {
-        fs.appendFileSync('./build.gradle', line.toString() + os.EOL);
-        if (/.*\ dependencies \{.*/.test(line)) {
-            fs.appendFileSync('./build.gradle', classpathsStr + os.EOL);
+    const lineReader = readline.createInterface({
+        terminal: false,
+        input: fs.createReadStream(rootBuildGradlePath)
+    });
+    lineReader.on("line", function (line) {
+        if (!line.includes(classpathsStrToVerify)) {
+            fs.appendFileSync('./build.gradle', line.toString() + os.EOL);
+            if (/.* dependencies \{.*/.test(line)) {
+                fs.appendFileSync('./build.gradle', classpathsStr + os.EOL);
+            }
         }
-    }
-    
-}).on("close", function () {
-    fs.rename('./build.gradle', rootBuildGradlePath, deferral.resolve);
-});
 
-var lineReaderApp = readline.createInterface({
-    terminal: false,
-    input : fs.createReadStream(appBuildGradlePath)
-});
-lineReaderApp.on("line", function (line) {
-    if (line.includes(googleServicesStr)) {
-        googleServicesStrExists = true;
-    }
-});    
-lineReaderApp.on("close", function () {
-    if (!googleServicesStrExists) {
-        fs.appendFileSync('./' + appBuildGradlePath, googleServicesStr + os.EOL);
-        fs.rename('./' + appBuildGradlePath, appBuildGradlePath, deferral.resolve);    
-    }
-    
-});
+    }).on("close", function () {
+        fs.rename('./build.gradle', rootBuildGradlePath, deferral.resolve);
+    });
 
-return deferral.promise;
+    const lineReaderApp = readline.createInterface({
+        terminal: false,
+        input: fs.createReadStream(appBuildGradlePath)
+    });
+    lineReaderApp.on("line", function (line) {
+        if (line.includes(perfPluginStr)) {
+            perfPluginStrExists = true;
+        }
+    });
+    lineReaderApp.on("close", function () {
+        if (!perfPluginStrExists) {
+            fs.appendFileSync('./' + appBuildGradlePath, perfPluginStr + os.EOL);
+            fs.rename('./' + appBuildGradlePath, appBuildGradlePath, deferral.resolve);
+        }
+
+    });
+
+    return deferral.promise;
 };
